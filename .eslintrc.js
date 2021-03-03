bf858f7a96641253d8e20e5c5625668b69a0d758
Fredrik Makila <fredrik@takanoha.io>
2021-03-03 21:56:05 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {},
+  env: {
+    browser: true,
+    node: true,
+  },
+};
